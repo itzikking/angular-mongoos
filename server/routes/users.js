ed1872a417913bbab679f91f3986864ceb4ddf67
itzik savaia <44684932+itzikking@users.noreply.github.com>
2019-10-17 17:38:50 +0300

    update

--- server/routes/users.js
+++ server/routes/users.js
@@ -5,7 +5,7 @@ var router = express.Router();
 const PostsSchema = require('../models/psots');
 const User = require('../models/user');
 const author = require('../models/author');
-//Middleware
+//Middlewares
 const controller = require('../controller');
 const Token = require('../middleware/Token');
 const Singup = require('../middleware/ChackUsers');
@@ -13,7 +13,7 @@ const Singup = require('../middleware/ChackUsers');
 
 router.post('/register', [Singup.ChackUsers], controller.Post_register);
 
-router.post('/login', controller.Post_login);
+router.post('/login', [Singup.ChackUsers, Token], controller.Post_login);
 
 
 module.exports = router;
